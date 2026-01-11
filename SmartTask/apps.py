854a4c0992a152328a1e2cc87bf32c321e3754e3
Ashok <avbhavle@gmail.com>
2026-01-11 22:33:37 +0530

    Started project

---
+++ SmartTask/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SmarttaskConfig(AppConfig):
+    name = 'SmartTask'
---
+++ SmartTask/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def logout(request):
+    return render(request,'logout.html')
+
+def forgot(request):
+    return render(request,'forgot.html')
