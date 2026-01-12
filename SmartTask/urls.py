c7f2ca1b26d5a5263804373cde234c9079210dea
Ashok <avbhavle@gmail.com>
2026-01-12 12:33:55 +0530

    added backends.py for authentication

--- SmartTask/urls.py
+++ SmartTask/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('forgot/', views.forgot, name='forgot'),
--- SmartTask/views.py
+++ SmartTask/views.py
@@ -1,14 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as auth_login
+
+
+@login_required(login_url='/login/')
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
-def login(request):
-    return render(request,'login.html')
+from django.contrib.auth import authenticate, login, logout
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            email=email,
+            password=password
+        )
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/home/')
+        else:
+            messages.error(request, "Invalid credentials")
+            return redirect('/login/')
+
+    return render(request, 'login.html')
+
 
 def register(request):
-    return render(request,'register.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        # phone=request.POST.get('phone')
+
+        # validations
+
+        
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists")
+            return redirect('/register/')
+
+        # create user
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        messages.success(request, "Account created successfully")
+        return redirect('/login/')
+
+    return render(request, 'register.html')
 
 def profile(request):
     return render(request,'profile.html')
@@ -18,3 +69,7 @@ def forgot(request):
 
 def manage_task(request):
     return render(request,'manage_task.html')
+
+def logout(request):
+    logout(request)
+    return redirect('/login/')
